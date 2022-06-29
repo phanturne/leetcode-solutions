a24788b9500de07b5edab36ddf6572c3a50d3726
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-06-28 23:29:46 -0700

    Update ReverseInteger.cpp

--- Solutions/ReverseInteger.cpp
+++ Solutions/ReverseInteger.cpp
@@ -13,12 +13,14 @@ public:
             int nextDigit = x % 10;
             x /= 10;
             
-            // Check for overflow. Last digit of (2^31 - 1 is 7
+            // Check for overflow. 2^31 - 1 = 2147483647, -2^31 = -2147483648
             if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && nextDigit > 7)) return 0;
             if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && nextDigit < -8)) return 0;
             
+            // Append the digit to the reversed int
             rev = rev * 10 + nextDigit;
         }
+        
         return rev;
     }
 };
