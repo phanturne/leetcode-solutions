898cf5d58ffe448d367c1d6c01fa5c6ca981ad17
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-06-29 13:14:34 -0700

    Create StringToInteger.cpp

---
+++ Solutions/StringToInteger.cpp
@@ -0,0 +1,37 @@
+// Leetcode Problem #8: https://leetcode.com/problems/string-to-integer-atoi/
+// Source: https://leetcode.com/problems/string-to-integer-atoi/solution/
+
+// Solution 1: Follow the Rules | O(n) time, O(1) space
+class Solution {
+public:
+    int myAtoi(string s) {
+        int result = 0, index = 0, sign = 1, n = s.length();
+
+        // Read and ignore leading whitespace
+        while (index < n && s[index] == ' ') index++;
+
+        // Check if the next char is "-" or "+" and update the sign
+        if (index < n && s[index] == '-') {
+            sign = -1;
+            index++;
+        }
+        else if (s[index] == '+') index++;
+
+        // Read in the next chars until the next non-digit char is reached.
+        while (index < n && isdigit(s[index])) {
+            int digit = s[index++] - '0';
+
+            // Check for overflow and underflow. 
+            if (result > INT_MAX / 10 || result == INT_MAX / 10 && digit > INT_MAX % 10) {
+                return sign == 1 ? INT_MAX : INT_MIN;
+            }
+
+            // Add the digit to the end of our int. Leading 0s are ignored because 0 * 10 + 0 = 0
+            result = result * 10 + digit;
+        }
+
+        // The number did not overflow/underflow. Return it after multiplying its sign.
+        return sign * result;
+    }
+};
+
