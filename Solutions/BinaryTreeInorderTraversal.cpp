ca51637b79236936fa717d335b762957ce028091
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-08-05 17:53:41 -0700

    Update BinaryTreeInorderTraversal.cpp

--- Solutions/BinaryTreeInorderTraversal.cpp
+++ Solutions/BinaryTreeInorderTraversal.cpp
@@ -32,5 +32,35 @@ public:
 };
 
 // Solution 2 (Iterate with Stack) | O(n) time, O(n) space
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;            // Vector to hold the answer
+        stack<TreeNode*> stack;     // Stack to simulate recursion
+        TreeNode* curr = root;
+        
+        // While the current node exists or stack is not empty
+        while (curr || !stack.empty()) {
+            // While the current node exists
+            while (curr) {
+                // Push current node to stack and set current node to its left child
+                stack.push(curr);
+                curr = curr->left;
+            }
+                
+            // Pop from stack and set current node to the popped node
+            curr = stack.top();
+            stack.pop();
+            
+            // Add current node to vector
+            ans.push_back(curr->val);
+            
+            // Set current node to its right child
+            curr = curr->right;
+        }
+             
+        return ans; // Return answer
+    }
+};
 
 // Solution 3 (Morris Traversal) | O(n) time, O(1) space
