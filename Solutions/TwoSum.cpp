c708f1533223f5b7ed113a9ac9a9f7ac0a36eb48
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-09-03 08:33:54 -0700

    Update TwoSum.cpp

--- Solutions/TwoSum.cpp
+++ Solutions/TwoSum.cpp
@@ -1,24 +1,23 @@
 // Leetcode Problem #1: https://leetcode.com/problems/two-sum/
 // Source: https://leetcode.com/problems/two-sum/discuss/13/Accepted-C++-O(n)-Solution/263
 
-// Solution 1: Hashmap | O(n) time, O(n) space
+// Solution 1: Map | O(n) time, O(n) space
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Create an unordered map
-        unordered_map<int, int> hashTable;
+        // Create a map of values w/ indices of the elements "looking for it"
+        unordered_map<int, int> map;
         
         // For each number in nums
         for (int i = 0; i < nums.size(); i++) {
-            // If target - currNum is in the map:
-            auto it = hashTable.find(target - nums[i]);
-            if (it != hashTable.end()) {
-                return vector<int> {it->second, i};
-            }
+            // If the element is in the map, return the indices
+            auto it = map.find(nums[i]);
+            if (it != map.end()) return {it->second, i};
 
-            // Else add currNum and its index to the map
-            hashTable[nums[i]] = i;
+            // Else store target - current element with the current index
+            map[target - nums[i]] = i;
         }
-        return {}; // Solution not found, return empty set
+        
+        return {}; // Solution not found, return empty vector. SHOULD NOT HAPPEN!
     }
 };
