6d89833fdd7904a296f543cf798c627a1ba11817
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-09-03 09:23:05 -0700

    Update ValidParenthesis.cpp

--- Solutions/ValidParenthesis.cpp
+++ Solutions/ValidParenthesis.cpp
@@ -5,28 +5,25 @@
 class Solution {
 public:
     bool isValid(string s) {
-        // Stack to keep track of order of brackets
+        // Create a stack to track the order of the brackets
         stack<char> stack;
+        
+        // Create a map from open to closing brackets
+        unordered_map<char, char> map = { {'(', ')'}, {'{', '}'}, {'[', ']'}};
         
         // Loop through the string
         for (char c : s) {
-            // If it is an open bracket, push it on the stack
-            if (c == '(') {
-                stack.push(')');
-            } else if (c == '{') {
-                stack.push('}');
-            } else if (c == '[') {
-                stack.push(']');
-            } else if (stack.empty() || stack.top() != c) {
-                // If it is a closing bracket, return false if there is no match or if the stack is empty
-                return false;
-            } else {
-                // Otherwise the brackets match, so just pop from the stack
-                stack.pop();
-            }
+            // If it is an open bracket, push the corresponding closing bracket onto the stack
+            if (c == '(' || c == '{' || c == '[') stack.push(map[c]);
+            
+            // If it is a closing bracket, return false if it doesn't match or if the stack is empty
+            else if (stack.empty() || stack.top() != c) return false;
+                
+            // Otherwise the brackets match, so just pop from the stack
+            else stack.pop();
         }
         
-        // If the stack is empty, all the brackets have been matched
-        return stack.empty(); 
+        // If stack is empty, all the brackets were matched
+        return stack.empty();
     }
 };
