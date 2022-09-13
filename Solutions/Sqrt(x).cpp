7dad58576a002d465024fdcd61a5851d78a85d69
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-09-13 10:48:21 -0700

    Update Sqrt(x).cpp

--- Solutions/Sqrt(x).cpp
+++ Solutions/Sqrt(x).cpp
@@ -2,43 +2,33 @@
 // Source 1: https://leetcode.com/problems/sqrtx/discuss/25057/3-4-short-lines-Integer-Newton-Every-Language
 // Source 2: https://leetcode.com/problems/sqrtx/discuss/25047/A-Binary-Search-Solution
 
-// Solution 1
+// Solution 1 (Binary Search) | O(logn) time, O(1) space
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0) return 0; // Base case
+        if (x <= 0) return x; 
         
         int left = 1, right = x; // Bounds for binary search
         
         // Loop while the root has not been found
-        // Note: Can also set condition to while (left <= right)
-        while (true) {
-            int mid = left + (right - left) / 2; // Calcultate mid
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
             
-            // If mid^2 > x, we're too far right
-            if (mid > (x / mid)) right = mid - 1;
-            
-            // Mid is <= x. If (mid + 1)^2 > x, return mid
-            else if ((mid + 1) > x / (mid + 1)) return mid;
-            
-            // Mid is < x AND (mid + 1)^2 < x, so we're too far left
-            else left = mid + 1;
+            if (mid <= x / mid) left = mid + 1; // Too small, search right half
+            else right = mid - 1; // Too big, search left half
         }
         
-        return -1;
+        return left - 1; // After exiting loop, left is 1 after the ans
     }
 };
-
-
-
 
 // Solution 2 (Newton's Method) | O( ? ) time, O(1) space
 class Solution {
 public:
     int mySqrt(int x) {
         long r = x;
-        while (r*r > x)
-            r = (r + x/r) / 2;
+        while (r * r > x)
+            r = (r + x / r) / 2;
         return r;
     }
 };
