c353480663c8a2aefae886e462a0ae0cb53bb755
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-09-17 14:02:11 -0700

    Create MaximumDepthOfBinaryTree.py

---
+++ Solutions/MaximumDepthOfBinaryTree.py
@@ -0,0 +1,14 @@
+# LC #104: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Solution 1 (Recursive) | O(n) time, O(n) space
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
