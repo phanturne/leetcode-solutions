6f95dac7f94964fd8524e02678081843d9dbed40
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-09-23 09:15:14 -0700

    Update MaximumSubarray.cpp

--- Solutions/MaximumSubarray.cpp
+++ Solutions/MaximumSubarray.cpp
@@ -1,6 +1,7 @@
 // Leetcode #53: https://leetcode.com/problems/maximum-subarray/
+// Source: https://leetcode.com/problems/maximum-subarray/discuss/1595195/C%2B%2BPython-7-Simple-Solutions-w-Explanation-or-Brute-Force-%2B-DP-%2B-Kadane-%2B-Divide-and-Conquer
 
-// Solution 1 (For Loop) | O(n) time, O(1) space
+// Solution 1 (Kadane's Algorithm) | O(n) time, O(1) space
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
