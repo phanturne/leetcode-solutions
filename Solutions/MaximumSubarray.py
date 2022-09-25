6fc67d758b1cf12b4cc36586fb1f49ae78f5d3cc
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-09-24 22:51:27 -0700

    Create MaximumSubarray.py

---
+++ Solutions/MaximumSubarray.py
@@ -0,0 +1,18 @@
+# LC #53: https://leetcode.com/problems/maximum-subarray/
+# Source: https://leetcode.com/problems/maximum-subarray/discuss/1595195/C%2B%2BPython-7-Simple-Solutions-w-Explanation-or-Brute-Force-%2B-DP-%2B-Kadane-%2B-Divide-and-Conquer
+
+# Solution 1 (Kadane's Algorithm) | O(n) time, O(1) space
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Track the currest sum and largest sum seen so far
+        currSum, maxSum = 0, nums[0] 
+        
+        # Loop through the array
+        for num in nums:
+            # Choose between adding num to currSum or starting fresh w/ only num
+            currSum = max(currSum + num, num)
+                
+            # Update the largest sum seen so far
+            maxSum = max(maxSum, currSum)
+            
+        return maxSum
