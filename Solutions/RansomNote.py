7f17fb4c1002bf4efde06dc79c5bc56c2bb05e7b
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-12-15 11:32:30 -0800

    Update RansomNote.py

--- Solutions/RansomNote.py
+++ Solutions/RansomNote.py
@@ -6,7 +6,7 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         return Counter(ransomNote) <= Counter(magazine)
       
-# Solution 2 (Dictionary) | O(m + n) time, O(1) space
+# Solution 2 (List) | O(m + n) time, O(1) space
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         dict = [0] * 26
@@ -22,4 +22,22 @@ class Solution:
             if dict[ord(letter) - ord('a')] < 0:
                 return False
             
+        return True
+
+# Solution 3 (Dictionary) | O(m + n) time, O(1) space
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        # Count the number of each letter in the magazine
+        magazineDict = {}
+        for c in magazine:
+            magazineDict[c] = magazineDict.get(c, 0) + 1
+
+        # For each letter in the ransomNote, subtract 1 from its count from the magazine
+        # If the count of any letter in the dict becomes < 0, the note can't be constructed
+        for c in ransomNote:
+            magazineDict[c] = magazineDict.get(c, 0) - 1
+            if magazineDict[c] < 0:
+                return False
+
+        # Otherwise the ransomNote can be constructed
         return True
