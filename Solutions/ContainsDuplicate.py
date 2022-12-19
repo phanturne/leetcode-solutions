b4cf92a09498d70532fccf4b2a1565de85a8cace
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2022-12-19 13:19:53 -0800

    Update ContainsDuplicate.py

--- Solutions/ContainsDuplicate.py
+++ Solutions/ContainsDuplicate.py
@@ -6,6 +6,12 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(nums) > len(set(nums))
       
+# Solution 1b (collections.Counter) | O(n) time, O(n) space
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hasDup = sum((val > 1) for val in collections.Counter(nums).values())
+        return hasDup
+    
 # Solution 2 (Brute Force - 2 Loops) | O(n^2) time, O(1) space
 
-# Solution 3 (Sort) | O(logn) time, O(1) space
+# Solution 3 (Sort) | O(nlogn) time, O(1) space
