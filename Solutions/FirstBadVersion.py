69e579acecf01dd75465693f7cbe572fe352f270
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2023-02-22 11:02:17 -0800

    Update FirstBadVersion.py

--- Solutions/FirstBadVersion.py
+++ Solutions/FirstBadVersion.py
@@ -21,3 +21,29 @@ class Solution:
                 left = mid + 1
         
         return right    # left and right will have same value, so we can return either
+
+    # Solution 2 (Vanilla Binary Search w/ Boundary Index) | O(logn) time, O(1) space
+    class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        # Initialize the search range
+        left, right = 1, n
+        
+        # Initialize the boundary index to -1, in case there are no bad versions
+        boundary_index = -1
+        
+        # Use binary search to find the first bad version
+        while (left <= right):
+            # Calculate the mid-point of the search range
+            mid = (left + right) // 2
+            
+            # If the mid-point version is bad, it means the first bad version could be either at the mid-point or to its left. 
+            if isBadVersion(mid):
+                boundary_index = mid
+                right = mid - 1
+            
+            # If the mid-point version is not bad, it means the first bad version must be to the right of the mid-point. 
+            else:
+                left = mid + 1
+        
+        # Return the boundary index, which is the first bad version found during the search
+        return boundary_index
